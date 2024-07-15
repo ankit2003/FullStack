7d4bba63d59ae7bcabbfb799b3162d58a5e33840
Ankit <ankitgupta2069@gmail.com>
2024-07-15 22:53:41 +0900

    Added bootstrap

--- Frontend/my-react-app/src/index.js
+++ Frontend/my-react-app/src/index.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import Blogs from "./pages/Blogs";
 import Contact from "./pages/Contact";
 import NoPage from "./pages/NoPage";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
   return (
@@ -21,5 +22,5 @@ export default function App() {
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
