042d1cd4167e6c14c8e205ee8b072ddd7571e738
Ankit <ankitgupta2069@gmail.com>
2024-07-16 23:28:32 +0900

    removed the deleted file link

--- Frontend/my-react-app/src/App.js
+++ Frontend/my-react-app/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
-import Blogs from "./pages/Blogs";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Default from "./components/Default";
